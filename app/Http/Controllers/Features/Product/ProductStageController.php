0730cc338227ac37b58ed12593c0eefbdd8ed82c
Mojahidul Islam <bdemon245@gmail.com>
2024-01-04 22:42:37 +0600

    added migration, controller, model for product stage and category

---
+++ app/Http/Controllers/Features/Product/ProductStageController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Features\Product;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductStageController extends Controller
+{
+    //
+}
---
+++ database/migrations/2024_01_04_163139_create_product_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('owner_id');
+            $table->foreign('owner_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->string('name');
+            $table->string('text_color')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_categories');
+    }
+};

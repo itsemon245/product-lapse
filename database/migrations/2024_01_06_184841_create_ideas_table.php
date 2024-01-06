fd20dff2fac71e0ce86e9bec7727054f74b4743f
mdparvej45 <pj.parvaz45@gmail.com>
2024-01-07 00:57:41 +0600

    Create Idea model & migration with schema.

---
+++ database/migrations/2024_01_06_184841_create_ideas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ideas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('owner_id');
+            $table->foreign('owner_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->string('name');
+            $table->string('owner');
+            $table->enum('priority', ['first', 'second']);
+            $table->longText('details');
+            $table->longText('requirements');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ideas');
+    }
+};

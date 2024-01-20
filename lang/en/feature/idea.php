065113a61988e1e95ad0e6372154b2ded61ad00a
mdparvej45 <pj.parvaz45@gmail.com>
2024-01-20 19:37:08 +0600

    SET Idea pages Localization key.

--- lang/en/feature/idea.php
+++ lang/en/feature/idea.php
@@ -1,9 +1,41 @@
 <?php
 return [
+
+    //list page
     'title' => 'Innovate',
     'add' => 'Add Innovate',
     'search' => 'Search Innovate',
     'select' => 'Showing Innovate',
+
+    //add page
+    'title-add' => 'Add Idea',
+    'name-input' => 'Idea name',
+    'owner-input' => 'Idea Owner', // + details page
+    'priority-input' => 'Priority', // + details page
+    'requirements-input' => 'Idea requirements', // + details page
+    'cancel-btn' => 'Cancel',
+
+
+    //details page
+    'title-details' => 'Idea details',
+    'priority-1' => 'New idea',
+    'priority-2' => 'Evaluate idea',
+    'priority-3' => 'Discuss idea',
+    'priority-4' => 'Final wording',
+    'priority-5' => 'Accept idea',
+    'priority-6' => 'Refuse idea',
+    'title-page' => 'Man perfume',
+    'update-btn' => 'Update',
+    'change-btn' => 'Convert to change request',
+    'task-btn' => 'Convert fo task for execution',
+    'comments' => 'Comments',
+    'write-comment' => 'Write Comment',
+
+    //Edit page
+    'title-edit' => 'Edit Idea',
+    
+
+ 
 
 ]
 
--- lang/en/feature/product.php
+++ lang/en/feature/product.php
@@ -5,8 +5,13 @@ return [
     'add' => 'Add product',
     'search' => 'Search product',
     'select' => 'Showing products',
+    'name-input' => 'Product name',
+    'category-input' => 'Category',
+    'url-input' => 'URL',
+    'stage-input' => 'Product stage',
+    'logo-input' => 'Product logo',
+    'description' => 'Description',
+    'cancel-btn' => 'Cancel',
+
 ];
 
-
-
-?>

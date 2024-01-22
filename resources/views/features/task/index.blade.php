3a9fa8f763ad2ba1c7827612dfdb068e29032853
Msr Nayeem <msrnayeem@gmail.com>
2024-01-23 02:33:44 +0600

    minor in task

--- resources/views/features/task/index.blade.php
+++ resources/views/features/task/index.blade.php
@@ -12,8 +12,7 @@
         </x-slot:search>
 
         <x-slot:actions>
-            <x-button hx-get="{{ route('support.create') }}" hx-push-url="true" hx-target="#hx-global-target"
-                hx-select="#hx-global-target">
+            <x-button href="{{ route('task.create') }}" type="link">
                 <i class="ti-plus"></i>
                 Add Task
             </x-button>
@@ -27,60 +26,68 @@
                     <option value="">Durable product</option>
                     <option value="">Initial idea</option>
                     <option value="">Stopped</option>
-                </select><div class="nice-select selectpickers selectpickers2" tabindex="0"><span class="current">All</span><ul class="list"><li data-value="" class="option selected focus">All</li><li data-value="" class="option">Durable product</li><li data-value="" class="option">Initial idea</li><li data-value="" class="option">Stopped</li></ul></div>
+                </select>
+                <div class="nice-select selectpickers selectpickers2" tabindex="0"><span class="current">All</span>
+                    <ul class="list">
+                        <li data-value="" class="option selected focus">All</li>
+                        <li data-value="" class="option">Durable product</li>
+                        <li data-value="" class="option">Initial idea</li>
+                        <li data-value="" class="option">Stopped</li>
+                    </ul>
+                </div>
             </form>
         </x-slot:filter>
 
         <x-slot:list>
             @foreach ($tasks as $task)
-            <div class="col-md-6">
-                <div class="item lon new">
-                    <div class="list_item">
-                        <figure><a href="#"><img src="img/p6.png" alt=""></a></figure>
-                        <div class="joblisting_text">
-                            <div class="job_list_table">
-                                <div class="jobsearch-table-cell">
-                                    <h4><a href="{{ route('task.show', ['task' => base64_encode($task->id)]) }}"
-                                            class="f_500 t_color3">{{ $task->name }}</a>
-                                    </h4>
-                                    <ul class="list-unstyled">
-                                        <li class="p_color3">@lang('task.' . $task->status)</li>
-                                        <li>
-                                            {{ \Carbon\Carbon::parse($task->created_at)->format('l, j F Y') }}
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="jobsearch-table-cell">
-                                    <div class="jobsearch-job-userlist">
-                                        <div class="like-btn">
-                                            <form
-                                                action="{{ route('task.destroy', ['task' => base64_encode($task->id)]) }}"
-                                                method="POST">
-                                                @csrf
-                                                @method('DELETE')
+                <div class="col-md-6">
+                    <div class="item lon new">
+                        <div class="list_item">
+                            <figure><a href="#"><img src="img/p6.png" alt=""></a></figure>
+                            <div class="joblisting_text">
+                                <div class="job_list_table">
+                                    <div class="jobsearch-table-cell">
+                                        <h4><a href="{{ route('task.show', ['task' => base64_encode($task->id)]) }}"
+                                                class="f_500 t_color3">{{ $task->name }}</a>
+                                        </h4>
+                                        <ul class="list-unstyled">
+                                            <li class="p_color3">@lang('task.' . $task->status)</li>
+                                            <li>
+                                                {{ \Carbon\Carbon::parse($task->created_at)->format('l, j F Y') }}
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="jobsearch-table-cell">
+                                        <div class="jobsearch-job-userlist">
+                                            <div class="like-btn">
+                                                <form
+                                                    action="{{ route('task.destroy', ['task' => base64_encode($task->id)]) }}"
+                                                    method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
 
-                                                <button type="submit" class="shortlist" title="Delete">
-                                                    <i class="ti-trash"></i>
-                                                </button>
-                                            </form>
+                                                    <button type="submit" class="shortlist" title="Delete">
+                                                        <i class="ti-trash"></i>
+                                                    </button>
+                                                </form>
+
+                                            </div>
+                                            <div class="like-btn">
+                                                <a href="{{ route('task.edit', ['task' => base64_encode($task->id)]) }}"
+                                                    class="shortlist" title="Edit">
+                                                    <i class="ti-pencil"></i>
+                                                </a>
+
+                                            </div>
 
                                         </div>
-                                        <div class="like-btn">
-                                            <a href="{{ route('task.edit', ['task' => base64_encode($task->id)]) }}"
-                                                class="shortlist" title="Edit">
-                                                <i class="ti-pencil"></i>
-                                            </a>
-
-                                        </div>
-
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
         </x-slot:list>
     </x-feature.index>
 @endsection

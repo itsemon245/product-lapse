0429a7e738309766c9f2333acca0c6b9e995641e
mdparvej45 <pj.parvaz45@gmail.com>
2024-01-23 18:20:57 +0600

    merge conflict.

--- resources/views/features/report/partials/create.blade.php
+++ resources/views/features/report/partials/create.blade.php
@@ -31,29 +31,17 @@
                         <x-input-error :messages="$errors->get('type')" class="mt-2" />
                     </div>
                     <div class="form-group text_box col-lg-6 col-md-6">
-<<<<<<< HEAD
-                        <x-input-label for="report_date" value="Report date" />
-                        <x-text-input id="report_date" class="block mt-1 w-full" type="text"
-                            placeholder="Enter report date" name="report_date" :value="old('report_date')" autofocus />
-                        <x-input-error :messages="$errors->get('report_date')" class="mt-2" />
-=======
                         <x-input-label for="report_date" value="{{ __('feature/report.label.date') }}" />
                         <x-input id="report_date" class="block mt-1 w-full" type="text"
                             placeholder="{{ __('feature/report.placeholder.date') }}" name="report_date" :value="old('report_date')"  autofocus />
->>>>>>> parvez
                     </div>
                     <div class="form-group text_box col-lg-6 col-md-6">
                         <x-attach label="{{ __('feature/report.label.upload') }}" name='file' />
                         <x-input-error :messages="$errors->get('file')" class="mt-2" />
                     </div>
                     <div class="form-group text_box col-lg-12 col-md-6">
-<<<<<<< HEAD
-                        <x-textarea placeholder="Write description" rows="5" cols="10" name="description"
-                            label="Description" />
-=======
                         <x-textarea placeholder="{{ __('feature/report.label.description') }}" rows="5" cols="10" name="description"
                             label="{{ __('feature/report.placeholder.description') }}" />                        
->>>>>>> parvez
                     </div>
 
                 </div>
@@ -62,17 +50,10 @@
                     <x-button type="submit">
                         @__('feature/report.submit')
                     </x-button>
-<<<<<<< HEAD
-            </form>
-            <x-btn-secondary name="Cancle" />
-            </div>
-
-=======
                 </form>
                     <x-btn-secondary name="{{ __('feature/report.cancel') }}" />
                 </div>
            
->>>>>>> parvez
         </x-slot:from>
     </x-feature.create>
 @endsection

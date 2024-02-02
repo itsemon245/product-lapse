ee728d1923ce7285c410c8943b354a7b1c1b6641
mdparvej45 <pj.parvaz45@gmail.com>
2024-02-02 23:05:16 +0600

    fix bugs.

--- resources/views/features/product/home.blade.php
+++ resources/views/features/product/home.blade.php
@@ -48,11 +48,6 @@
                 </div>
             </div>
             <div class="row">
-                {{-- @php
-                    
-                    dd( request()->segment(4));
-                @endphp --}}
-                {{-- {{   }} --}}
                 @foreach ($features as $feature)
                     <div class="col-lg-3 col-md-4 col-6">
                         <div class="box-item">

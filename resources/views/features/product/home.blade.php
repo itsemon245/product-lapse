6de7ff0b8285add450f3838e3b80dc1af67d790f
Mojahidul Islam <bdemon245@gmail.com>
2024-02-07 00:12:41 +0600

    lang and product home fixed

--- resources/views/features/product/home.blade.php
+++ resources/views/features/product/home.blade.php
@@ -24,21 +24,26 @@
                 <div class="listing_tab">
                     <div class="item lon new">
                         <div class="list_item">
-                            
+
                             <div class="joblisting_text">
                                 <div class="job_list_table">
                                     <div class="jobsearch-table-cell">
-                                        <h4>
-                                            <a href="{{ route('product.info', productId()) }}" class="f_500 t_color3">{{ $product->name }}</a>
-                                            <a href="{{ route('product.edit', productId()) }}" class="btn_hover agency_banner_btn btn-bg"><i
-                                                class="ti-pencil"></i> @__('productHome.edit')</a>
-                                            
+                                        <h4 class="justify-between items-center">
+                                            <a href="{{ route('product.info', $product->id) }}"
+                                                class="f_500 t_color3">{{ $product->name }}</a>
+                                            {{-- <a href="{{ route('product.edit', $product->id) }}" class="btn_hover agency_banner_btn btn-bg"><i
+                                                class="ti-pencil"></i> @__('productHome.edit')</a> --}}
+                                            <form action="{{ route('product.edit', $product) }}" method="get">
+                                                <button type="submit" class="btn_hover agency_banner_btn btn-bg"><i
+                                                        class="ti-pencil"></i> Edit product</button>
+                                            </form>
                                         </h4>
                                         <ul class="list-unstyled">
                                             <li class="p_color1">{{ $product->stage }}</li>
                                             <li>{{ $product->description }}</li>
-                                            <li>  <a href="{{ route('product.edit', productId()) }}" class="btn_hover agency_banner_btn btn-bg"><i
-                                                class="ti-pencil"></i> @__('productHome.edit')</a></li>
+                                            <li> <a href="{{ route('product.edit', $product->id) }}"
+                                                    class="btn_hover agency_banner_btn btn-bg"><i class="ti-pencil"></i>
+                                                    @__('productHome.edit')</a></li>
                                         </ul>
                                     </div>
 
@@ -52,7 +57,8 @@
                 @foreach ($features as $feature)
                     <div class="col-lg-3 col-md-4 col-6">
                         <div class="box-item">
-                            <span class="box-item-num {{ $feature['counter'] == null ? 'd-none' : '' }}">{{ $feature['counter'] }}</span>
+                            <span
+                                class="box-item-num {{ $feature['counter'] == null ? 'd-none' : '' }}">{{ $feature['counter'] }}</span>
                             <a href="{{ $feature['route'] }}"></a>
                             <img style="margin:auto; margin-bottom:1rem;" src="{{ asset($feature['icon']) }}">
                             <h5 class="f_600 t_color2">{{ $feature['name'] }}</h5>

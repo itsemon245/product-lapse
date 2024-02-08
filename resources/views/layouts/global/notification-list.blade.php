1567ab53b537cf02d164a716d0b0c55c13b5d505
mdparvej45 <pj.parvaz45@gmail.com>
2024-02-08 12:12:27 +0600

    implement notification.

--- resources/views/layouts/global/notification-list.blade.php
+++ resources/views/layouts/global/notification-list.blade.php
@@ -1,77 +1,32 @@
 <li class="nav-item dropdown submenu notifi-list">
     <a class="nav-link dropdown-toggle btn-bg btn-icon" href="#" role="button"
         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="ti-bell"></i><span class="notifi-num">25</span>
+        <i class="ti-bell"></i><span class="notifi-num">{{ count(auth()->user()->notifications) }}</span>
     </a>
     <ul class="dropdown-menu">
-        <li class="nav-item">
+        @foreach (auth()->user()->notifications as $notification)
+        <li class="nav-item {{ $notification->read_at == null ? 'bg-light' : '' }}">
+            @php
+                $user = App\Models\User::with('image')->find($notification->data['owner_id']);
+                // dd($user)
+            @endphp
             <a href="blog-grid.html" class="nav-link">
                 <div class="feedback_item">
                     <div class="feed_back_author">
                         <div class="media">
                             <div class="img">
-                                <img src="img/profile1.png" alt="">
+                                <img src="{{ $user->image->url ?? asset('img/profile1.png') }} " alt="">
                             </div>
                             <div class="media-body">
-                                <h5 class="f_500"><span>Ahmed Mahmoud</span> Add you to his list</h5>
-                                <h6 class="f_400">25 min ago</h6>
+                                <h5 class="f_500"><span>{{ $user->name . ' ' . '|' }} </span> {{ $notification->data['reason'] }} </h5>
+                                <h6 class="f_400"> {{ $notification->created_at->diffForHumans() }} </h6>
                             </div>
                         </div>
                     </div>
                 </div>
             </a>
         </li>
-        <li class="nav-item">
-            <a href="blog-grid.html" class="nav-link">
-                <div class="feedback_item">
-                    <div class="feed_back_author">
-                        <div class="media">
-                            <div class="img">
-                                <img src="img/profile1.png" alt="">
-                            </div>
-                            <div class="media-body">
-                                <h5 class="f_500"><span>Ahmed Mahmoud</span> Add you to his list</h5>
-                                <h6 class="f_400">25 min ago</h6>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="blog-grid.html" class="nav-link">
-                <div class="feedback_item">
-                    <div class="feed_back_author">
-                        <div class="media">
-                            <div class="img">
-                                <img src="img/profile1.png" alt="">
-                            </div>
-                            <div class="media-body">
-                                <h5 class="f_500"><span>Ahmed Mahmoud</span> Add you to his list</h5>
-                                <h6 class="f_400">25 min ago</h6>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="blog-grid.html" class="nav-link">
-                <div class="feedback_item">
-                    <div class="feed_back_author">
-                        <div class="media">
-                            <div class="img">
-                                <img src="img/profile1.png" alt="">
-                            </div>
-                            <div class="media-body">
-                                <h5 class="f_500"><span>Ahmed Mahmoud</span> Add you to his list</h5>
-                                <h6 class="f_400">25 min ago</h6>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </li>
+        @endforeach
         <li class="nav-item text-center"><a href="#" class="nav-link">View all</a></li>
     </ul>
 </li>

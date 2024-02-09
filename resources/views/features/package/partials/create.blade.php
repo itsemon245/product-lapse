69993cd8fec3d8504dba75db0e62e47d393b8036
Mojahidul Islam <bdemon245@gmail.com>
2024-02-10 02:20:45 +0600

    replaced text-input with input and commented the conflicted package update route

--- resources/views/features/package/partials/create.blade.php
+++ resources/views/features/package/partials/create.blade.php
@@ -13,25 +13,25 @@
                         <div class="row">
                             <div class="form-group text_box col-lg-4 col-md-6">
                                 <x-input-label for="name" value="Package name" />
-                                <x-text-input id="name" class="block mt-1 w-full" type="text"
+                                <x-input id="name" class="block mt-1 w-full" type="text"
                                     placeholder="Enter package name" name="name" :value="old('name')" autofocus />
                                 <x-input-error :messages="$errors->get('name')" class="mt-2" />
                             </div>
                             <div class="form-group text_box col-lg-4 col-md-6">
                                 <x-input-label for="price" value="Package price" />
-                                <x-text-input id="price" class="block mt-1 w-full" type="text"
+                                <x-input id="price" class="block mt-1 w-full" type="text"
                                     placeholder="Enter package price" name="price" :value="old('price')" autofocus />
                                 <x-input-error :messages="$errors->get('price')" class="mt-2" />
                             </div>
                             <div class="form-group text_box col-lg-4 col-md-6">
                                 <x-input-label for="monthly_rate" value="Monthly rate" />
-                                <x-text-input id="monthly_rate" class="block mt-1 w-full" type="text"
+                                <x-input id="monthly_rate" class="block mt-1 w-full" type="text"
                                     placeholder="Enter monthly rate" name="monthly_rate" :value="old('monthly_rate')" autofocus />
                                 <x-input-error :messages="$errors->get('monthly_rate')" class="mt-2" />
                             </div>
                             <div class="form-group text_box col-lg-4 col-md-6">
                                 <x-input-label for="annual_rate" value="Annual rate" />
-                                <x-text-input id="annual_rate" class="block mt-1 w-full" type="text"
+                                <x-input id="annual_rate" class="block mt-1 w-full" type="text"
                                     placeholder="Enter annual rate" name="annual_rate" :value="old('annual_rate')" autofocus />
                                 <x-input-error :messages="$errors->get('annual_rate')" class="mt-2" />
                             </div>
@@ -51,25 +51,25 @@
                             </div>
                             <div class="form-group text_box col-lg-4 col-md-6">
                                 <x-input-label for="features" value="Features" />
-                                <x-text-input id="features" class="block mt-1 w-full" type="text"
+                                <x-input id="features" class="block mt-1 w-full" type="text"
                                     placeholder="Enter features" name="features" :value="old('features')" autofocus />
                                 <x-input-error :messages="$errors->get('features')" class="mt-2" />
                             </div>
                             <div class="form-group text_box col-lg-4 col-md-6">
                                 <x-input-label for="product_limit" value="Product limit" />
-                                <x-text-input id="product_limit" class="block mt-1 w-full" type="date"
+                                <x-input id="product_limit" class="block mt-1 w-full" type="date"
                                     name="product_limit" :value="old('product_limit')" autofocus />
                                 <x-input-error :messages="$errors->get('product_limit')" class="mt-2" />
                             </div>
                             <div class="form-group text_box col-lg-4 col-md-6">
                                 <x-input-label for="validity" value="Validity" />
-                                <x-text-input id="validity" class="block mt-1 w-full" type="date" name="validity"
+                                <x-input id="validity" class="block mt-1 w-full" type="date" name="validity"
                                     :value="old('validity')" autofocus />
                                 <x-input-error :messages="$errors->get('validity')" class="mt-2" />
                             </div>
                             <div class="form-group text_box col-lg-4 col-md-6">
                                 <x-input-label for="has_limited_features" value="Limited features" />
-                                <x-text-input id="has_limited_features" class="block mt-1 w-full" type="text"
+                                <x-input id="has_limited_features" class="block mt-1 w-full" type="text"
                                     name="has_limited_features" placeholder="Limited features" :value="old('has_limited_features')"
                                     autofocus />
                                 <x-input-error :messages="$errors->get('has_limited_features')" class="mt-2" />

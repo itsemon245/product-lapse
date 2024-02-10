58eaa36c819bb35a3606e3db83b39365be4d085d
mdparvej45 <pj.parvaz45@gmail.com>
2024-02-10 12:37:40 +0600

    fixed navigation bar.

--- resources/views/layouts/subscriber/app.blade.php
+++ resources/views/layouts/subscriber/app.blade.php
@@ -14,11 +14,58 @@
 
 <body class="relative overflow-x-hidden">
     {{-- Preloader --}}
-    {{-- @include('layouts.frontend.preloader') --}}
+    @include('layouts.frontend.preloader')
     <div class="" id="tolink-1">
         <div class="top-link"><a href="#tolink-1"><i class="ti-angle-up"></i></a></div>
 
-        @include('layouts.frontend.header')
+        <header class="header_area header_area_inner">
+            <nav class="navbar navbar-expand-lg menu_one menu_four">
+                <div class="container custom_container p0">
+                    <a class="navbar-brand" href="/">
+                        <img src="{{ asset('img/logo-white.png') }}" class="logo1" srcset="{{ asset('img/logo-white.png') }} 2x"
+                            alt="logo">
+                        <img src="{{ asset('img/logo.png') }}" class="logo2" srcset="{{ asset('img/logo.png') }} 2x"
+                            alt="logo">
+                    </a>
+                    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="menu_toggle">
+                            <span class="hamburger">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </span>
+                            <span class="hamburger-cross">
+                                <span></span>
+                                <span></span>
+                            </span>
+                        </span>
+                    </button>
+                    <ul class="navbar-nav menu hidden-sm hidden-xs navbar-nav-signin gap-4 items-center">
+                        <li class="nav-item dropdown submenu m-0">
+                            @include('layouts.frontend.locale-switcher')
+                        </li>
+                        @auth
+        
+                            <li class="nav-item dropdown submenu m-0">
+                                <a class="nav-link dropdown-toggle btn-bg btn-icon" href="{{ route('dashboard') }}" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="ti-user"></i>
+                                </a>
+                                @include('layouts.global.profile-menu')
+                            </li>
+                            @include('layouts.global.notification-list')
+                        @else
+                            <li class="nav-item dropdown submenu m-0">
+                                <a class="btn_get btn_hover hidden-sm hidden-xs btn-bg" href="{{ route('login') }}">login</a>
+                            <li>
+                            @endauth
+        
+                    </ul>
+                </div>
+            </nav>
+        </header>
 
         <div class="flex bg-gray-50 dark:bg-gray-900" :class="{ 'overflow-hidden': isSideMenuOpen }">
             @yield('sidebar')

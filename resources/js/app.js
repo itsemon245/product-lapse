a49a9c99df6ac140bc7f70278da73c3743905890
Mojahidul Islam <bdemon245@gmail.com>
2024-02-11 23:18:31 +0600

    build

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,8 @@
 import "./bootstrap";
 import "flowbite";
-import Cipher from "./services/cipher";
 
 import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
-window.Cipher = Cipher;
 
 Alpine.start();

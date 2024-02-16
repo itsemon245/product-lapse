d64167f4b2586a8907a26d56891f465006caf16f
Mojahidul Islam <bdemon245@gmail.com>
2024-02-17 00:10:35 +0600

    localize mobile nav

--- resources/views/layouts/frontend/navigation.blade.php
+++ resources/views/layouts/frontend/navigation.blade.php
@@ -2,64 +2,64 @@
     <ul class="navbar-nav menu pl_120">
         <li class="nav-item">
             <a class="nav-link" href="#tolink-1">
-                Home
+                @__('navigation.navbar.home')
             </a>
         </li>
         @auth
             <li class="nav-item hidden-md visible-sm">
                 <a class="nav-link" href="#tolink-6">
-                    Notifications
+                    @__('navigation.notifications')
                     <span class="notifi-num notifi-num2">25</span>
                 </a>
             </li>
             <li class="nav-item dropdown submenu hidden-md visible-sm">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="false">
-                    Profile
+                    @__('navigation.navbar.login')
                 </a>
                 @include('layouts.global.profile-menu')
             </li>
         @endauth
         <li class="nav-item">
             <a class="nav-link" href="#tolink-2">
-                About us
+                @__('navigation.navbar.about')
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#tolink-3">
-                Features
+                @__('navigation.navbar.features')
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#tolink-4">
-                Packages
+                @__('navigation.navbar.packages')
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#tolink-5">
-                Faq
+                @__('navigation.navbar.features')
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#tolink-6">
-                Contact us
+                @__('navigation.navbar.contact')
             </a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item hidden-md ">
             <form action="{{ route('lang.toggle') }}" method="post">
                 @csrf
                 @if (app()->getLocale() == 'ar')
-                    <button class="nav-link">
+                    <button class="nav-link w-full text-start rtl:text-end">
                         English
                     </button>
                 @else
-                    <button class="nav-link">
+                    <button class="nav-link w-full text-start rtl:text-end ">
                         عربى
                     </button>
                 @endif
             </form>
         </li>
-        <li class="nav-item"><a class="btn_get btn_hover hidden-md visible-sm" href="{{route('login')}}">login</a>
+        <li class="nav-item"><a class="btn_get btn_hover hidden-md visible-sm" href="{{route('login')}}">@__('navigation.navbar.login')</a>
         </li>
     </ul>
 </div>

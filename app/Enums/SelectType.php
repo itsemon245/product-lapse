d44dff46940022a1d3380e7dd34f95e3f9d256ec
Mojahidul Islam <bdemon245@gmail.com>
2024-02-17 22:54:03 +0600

    fix: SelectType Enum

--- app/Enums/SelectType.php
+++ app/Enums/SelectType.php
@@ -4,6 +4,7 @@ namespace App\Enums;
 enum SelectType: string
 {
     case CATEGORY = 'category';
+    case STAGE = 'stage';
     case STATUS = 'status';
     case CLASSIFICATION = 'classification';
     case PRIORITY = 'priority';

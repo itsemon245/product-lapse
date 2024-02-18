9b304ebe1f4723248fc9943247660097238f1f59
Mojahidul Islam <bdemon245@gmail.com>
2024-02-19 00:35:43 +0600

    fix breadcrumb

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -1,20 +1,22 @@
 @props(['list' => []])
 @if (count($list) > 0)
-<section class="breadcrumb_area">
-    <div class="container d-flex">
-        <div class="breadcrumb_content text-center ml-auto">
-            <ul class="breadcrumb">
-                @forelse ($list as $item)
-                    <li class="breadcrumb-item {{ request()->routeIs('dashboard') ? 'active' : '' }} "><a href="{{ route('dashboard') }}">@__('root.dashboard')</a></li>
-                    @if ($loop->last)
-                    <li class="breadcrumb-item active">{{ $item['label'] }}</li>
-                    @else
-                    <li class="breadcrumb-item"><a href="{{ $item['route'] }}">{{ $item['label'] }}</a></li>
-                    @endif
-                @empty
-                @endforelse
-            </ul>
+    <section class="breadcrumb_area">
+        <div class="container d-flex">
+            <div class="breadcrumb_content text-center ml-auto">
+                <ul class="breadcrumb">
+                    @forelse ($list as $item)
+                        <li class="breadcrumb-item {{ request()->routeIs('dashboard') ? 'active' : '' }} "><a
+                                href="{{ productId() ? route('product.show', $product) : rotute('dashboard') }}">@__('root.dashboard')</a>
+                        </li>
+                        @if ($loop->last)
+                            <li class="breadcrumb-item active">{{ $item['label'] }}</li>
+                        @else
+                            <li class="breadcrumb-item"><a href="{{ $item['route'] }}">{{ $item['label'] }}</a></li>
+                        @endif
+                    @empty
+                    @endforelse
+                </ul>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 @endif

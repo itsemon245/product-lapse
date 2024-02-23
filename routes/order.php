cc4e9be154d7ee365469ca7d9a5e72af736c31bf
Mojahidul Islam <bdemon245@gmail.com>
2024-02-24 01:23:48 +0600

    added auth middleware to order routes

--- routes/order.php
+++ routes/order.php
@@ -1,19 +1,20 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Order\OrderController;
 use App\Http\Controllers\Order\PaymentController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('order/{package}/create', [OrderController::class, 'create'])->name('order.create');
-Route::get('order/{order}/select-method', [OrderController::class, 'selectMethod'])->name('order.selectMethod');
-Route::post('order/{order}', [OrderController::class, 'store'])->name('order.store');
-
-
+Route::middleware('auth')
+    ->group(function () {
+        Route::get('order/{package}/create', [ OrderController::class, 'create' ])->name('order.create');
+        Route::get('order/{order}/select-method', [ OrderController::class, 'selectMethod' ])->name('order.selectMethod');
+        Route::post('order/{order}', [ OrderController::class, 'store' ])->name('order.store');
 
 // Payment
-Route::any('payment-ipn', [PaymentController::class, 'ipn'])->name('payment.ipn');
-Route::any('payment-callback', [PaymentController::class, 'callback'])->name('payment.callback');
-Route::get('payment-success', [PaymentController::class, 'success'])->name('payment.success');
-Route::get('payment-failed', [PaymentController::class, 'failed'])->name('payment.failed');
+        Route::any('payment-ipn', [ PaymentController::class, 'ipn' ])->name('payment.ipn');
+        Route::any('payment-callback', [ PaymentController::class, 'callback' ])->name('payment.callback');
+        Route::get('payment-success', [ PaymentController::class, 'success' ])->name('payment.success');
+        Route::get('payment-failed', [ PaymentController::class, 'failed' ])->name('payment.failed');
+    })
 
 ?>

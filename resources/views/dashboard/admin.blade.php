6877a47bdd51a29cc6e57a4c355a0c4aeb66ba19
mdparvej45 <pj.parvaz45@gmail.com>
2024-02-29 01:58:50 +0600

    fixed admin panele Deashoard index.

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -75,10 +75,28 @@
                                     <div class="joblisting_text">
                                         <div class="job_list_table">
                                             <div class="jobsearch-table-cell">
-                                                <h4 class="mt-2">Subscriber</h4>
+                                                <h4 class="mt-2">Total Orders</h4>
+                                                <h3 class="text-center">{{ count($orders) ?? '0' }}</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-2">
+                        <div class="item lon new card">
+                            <div class="list_item p-4">
+                                <div class="d-flex gap-2">
+                                    <img style="width: 4rem;" class="rounded-circle border" src="{{ favicon() }}"
+                                        alt="">
+                                    <div class="joblisting_text">
+                                        <div class="job_list_table">
+                                            <div class="jobsearch-table-cell">
+                                                <h4 class="mt-2">Order Summary</h4>
                                                 <div class="d-flex text-muted">
-                                                    <p>Hello wordl</p> |
-                                                    <p>Hello wordl</p>
+                                                    <p>Completed: <span>{{ count($completedOrder) }}</span></p> 
+                                                    <p>Pending: <span>{{ count($pendingOrder) }}</span> </p>
                                                 </div>
                                             </div>
                                         </div>
@@ -96,32 +114,8 @@
                                     <div class="joblisting_text">
                                         <div class="job_list_table">
                                             <div class="jobsearch-table-cell">
-                                                <h4 class="mt-2">Subscriber</h4>
-                                                <div class="d-flex text-muted">
-                                                    <p>Hello wordl</p> |
-                                                    <p>Hello wordl</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4 mb-2">
-                        <div class="item lon new card">
-                            <div class="list_item p-4">
-                                <div class="d-flex gap-2">
-                                    <img style="width: 4rem;" class="rounded-circle border" src="{{ favicon() }}"
-                                        alt="">
-                                    <div class="joblisting_text">
-                                        <div class="job_list_table">
-                                            <div class="jobsearch-table-cell">
-                                                <h4 class="mt-2">Subscriber</h4>
-                                                <div class="d-flex text-muted">
-                                                    <p>Hello wordl</p> |
-                                                    <p>Hello wordl</p>
-                                                </div>
+                                                <h4 class="mt-2">Failed order</h4>
+                                                <h3 class="text-center">{{ count($failedOrder) ?? '0' }}</h3>
                                             </div>
                                         </div>
                                     </div>

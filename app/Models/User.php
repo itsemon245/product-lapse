55afdc9903685a403193a849af846d70a5f956bf
Mojahidul Islam <bdemon245@gmail.com>
2024-03-07 00:32:47 +0600

    fix: free plan

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function activePlan()
     {
-        return $this->plans()->whereDate('expired_at', '>=', now()->format('Y-m-d'))->where('active', true)->limit(1);
+        return $this->plans()->whereDate('expired_at', '>=', now()->format('Y-m-d'))->orWhere('expired_at', null)->where('active', true)->limit(1);
     }
 
     public function activePlanName()

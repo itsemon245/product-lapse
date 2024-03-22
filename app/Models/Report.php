2fe8cfb45d8fae020aafa64a5657fe93c1dcfdb5
Msr Nayeem <msrnayeem@gmail.com>
2024-03-22 22:22:01 +0600

    support

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -9,12 +9,13 @@ use App\Traits\HasCreator;
 use App\Traits\HasFile;
 use App\Traits\HasOwner;
 use App\Traits\HasProducts;
+use App\Traits\HasSelects;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Report extends Model
 {
-    use HasFactory, HasFile, HasProducts, HasComments, HasCreator, HasOwner, CanSendNotifications;
+    use HasFactory, HasFile, HasProducts, HasSelects, HasComments, HasCreator, HasOwner, CanSendNotifications;
     protected $guarded = [];
 
     public function user()
--- app/Models/Support.php
+++ app/Models/Support.php
@@ -7,12 +7,13 @@ use App\Traits\HasComments;
 use App\Traits\HasCreator;
 use App\Traits\HasOwner;
 use App\Traits\HasProducts;
+use App\Traits\HasSelects;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Support extends Model
 {
-    use HasFactory, HasProducts, HasComments, HasCreator, HasOwner, CanSendNotifications;
+    use HasFactory, HasProducts, HasSelects, HasComments, HasCreator, HasOwner, CanSendNotifications;
     protected $guarded = [];
 
 }

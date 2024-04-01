56f3e569982a2a4686b2887e351c702fa696d81c
Mojahidul Islam <bdemon245@gmail.com>
2024-04-02 00:22:13 +0600

    fix minor bug

--- database/migrations/2024_03_22_184037_update_slug_in_pages_table.php
+++ database/migrations/2024_03_22_184037_update_slug_in_pages_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('pages', function (Blueprint $table) {
-            $table->string('slug')->nullable();
+            $table->string('slug')->nullable()->change();
         });
     }
 

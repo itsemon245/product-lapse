0073e92bf49a5414e7e1751789bd20941efc7ad0
Mojahidul Islam <itsemon245@gmail.com>
2024-07-02 00:46:37 +0600

    fix: comment issue

--- app/Http/Controllers/Features/CommentController.php
+++ app/Http/Controllers/Features/CommentController.php
@@ -2,60 +2,62 @@
 
 namespace App\Http\Controllers\Features;
 
-use App\Models\Comment;
-use App\Models\Delivery;
-use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CommentRequest;
+use App\Models\Comment;
+use App\Models\Delivery;
 use App\Notifications\CommentNotification;
 use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Str;
 
 class CommentController extends Controller
 {
-
     public function store(CommentRequest $request)
     {
         if (str($request->commentable_type)->contains('App\\Models')) {
             $commentableType = $request->commentable_type;
         } else {
-            $modelName       = Str::contains($request->commentable_type, '\\') ? explode("\\", $request->commentable_type) : [ $request->commentable_type ];
-            $modelName       = array_pop($modelName);
-            $commentableType = "App\\Models\\" . Str::studly($modelName);
+            $modelName = Str::contains($request->commentable_type, '\\') ? explode('\\', $request->commentable_type) : [$request->commentable_type];
+            $modelName = array_pop($modelName);
+            $commentableType = 'App\\Models\\'.Str::studly($modelName);
         }
 
         $comment = Comment::create([
-            'user_id'          => auth()->id(),
-            'parent_id'        => $request->parent_id,
-            'body'             => $request->comment,
+            'user_id' => auth()->id(),
+            'parent_id' => $request->parent_id,
+            'body' => $request->comment,
             'commentable_type' => $commentableType,
-            'commentable_id'   => $request->commentable_id,
-         ]);
-         if ($commentableType == Delivery::class) {
+            'commentable_id' => $request->commentable_id,
+        ]);
+        if ($commentableType == Delivery::class) {
             $comment->status = 'pending';
             $comment->update();
-         }
-        [$users, $initiator, $feature] = getNotificationData(Comment::class, productId());
+        }
+        [$users, $initiator, $feature] = getNotificationData($comment->commentable, productId());
         Notification::send($users, new CommentNotification($initiator, $feature));
         notify()->success(__('notify/success.create'));
+
         return back();
     }
+
     public function delete(Comment $comment)
     {
 
     }
 
-
     public function toggleStatus(Comment $comment, string $status)
     {
-        if (!str($status)->contains(['pending', 'agreed', 'disagreed'])) {
+        if (! str($status)->contains(['pending', 'agreed', 'disagreed'])) {
             notify()->warning('Invalid Status!');
+
             return back();
         }
         $comment->update([
-            'status'=> $status
+            'status' => $status,
         ]);
         $status = str($status)->title();
         notify()->success(__("$status to this comment!"));
+
         return back();
 
     }
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,17 +2,21 @@
 
 namespace App\Models;
 
-use App\Models\User;
-use App\Traits\CanSendNotifications;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Comment extends Model
 {
-    use HasFactory, CanSendNotifications;
+    use HasFactory;
+
     protected $guarded = [];
+
+    public function commentable()
+    {
+        return $this->morphTo('commentable');
+    }
 
     public function user()
     {
@@ -23,9 +27,9 @@ class Comment extends Model
     {
         return $this->belongsTo(Comment::class, 'parent_id');
     }
+
     public function replies(): HasMany
     {
         return $this->hasMany(Comment::class, 'parent_id');
     }
-
 }
